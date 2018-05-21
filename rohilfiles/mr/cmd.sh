c183939db056f9c169b383246e96ecb7186651d5
Rohil <verma.rohil@gmail.com>
2018-05-21 01:28:11 +0000

    added montezuma

---
+++ rohilfiles/mr/cmd.sh
@@ -0,0 +1,1 @@
+/home/verma_rohil/noreward-rl/curiosity/bin/python train.py --default --env-id MontezumaRevenge-v0 -m nohup -l /scratch/mr
---
+++ rohilfiles/mricm/cmd.sh
@@ -0,0 +1,1 @@
+/home/verma_rohil/noreward-rl/curiosity/bin/python train.py --default --env-id MontezumaRevenge-v0 --noReward -m nohup -l /scratch/mricm
